ff4fd813390dd8c9793ef9047a25e35b29f2fd4f
lizejia <376489098@qq.com>
2018-07-12 00:37:58 +0800

    读取文件方式获取问题

--- UnitTest/UnitTest.cs
+++ UnitTest/UnitTest.cs
@@ -132,18 +132,7 @@ how much wood could a woodchuck chuck if a woodchuck could chuck wood ?";
         [TestMethod]
         public void TestAll()
         {
-            string input = @"glob is I
-prok is V
-pish is X
-tegj is L
-glob glob Silver is 34 Credits
-glob prok Gold is 57800 Credits
-pish pish Iron is 3910 Credits
-how much is pish tegj glob glob ?
-how many Credits is glob prok Silver ?
-how many Credits is glob prok Gold ?
-how many Credits is glob prok Iron ?
-how much wood could a woodchuck chuck if a woodchuck could chuck wood ?";
+            string input = Tool.ReadTxtContent("input.txt");
             MessageMain messageMain = new MessageMain();
             var output = messageMain.Start(input);
             string answer = @"pish tegj glob glob is 42
